72e2179fc19f059d69148a568388828b97e6074b
Selahattin Demir <selahattindemir686@gmail.com>
2022-01-08 14:37:42 +0300

    feat: add splash screen

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import Navigation from './Navigation';
 import {Provider, useDispatch} from 'react-redux';
 import {createStore, applyMiddleware} from 'redux';
 import ReduxThunk from 'redux-thunk';
+import SplashScreen from 'react-native-splash-screen';
 
 import reducer from './Context/reducer/reducer';
 
 const store = createStore(reducer, applyMiddleware(ReduxThunk));
 
 export default function App() {
+  React.useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+  
   return (
     <Provider store={store}>
       <Navigation />
