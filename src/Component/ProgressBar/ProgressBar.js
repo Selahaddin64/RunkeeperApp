b024280f9ae249d51cb63b10bbefb16934bdd7ff
Selahattin Demir <selahattindemir686@gmail.com>
2022-01-08 14:39:58 +0300

    feat: Add ProgressBar

---
+++ src/Component/ProgressBar/ProgressBar.js
@@ -0,0 +1,31 @@
+// Progress bar used in summary and running screen
+import React from 'react';
+import {View} from 'react-native';
+import styles from './ProgressBar.styles';
+
+const ProgressBar = ({
+  prog,
+  innerBorderColor,
+  containerBorderColor,
+  containerBgr,
+  propStyles,
+}) => {
+  return (
+    <View
+      style={{
+        ...styles.progressBarContainer,
+        borderColor: containerBorderColor,
+        backgroundColor: containerBgr,
+        ...propStyles,
+      }}>
+      <View
+        style={{
+          ...styles.progressBar,
+          width: prog,
+          borderColor: innerBorderColor,
+        }}></View>
+    </View>
+  );
+};
+
+export default ProgressBar;
