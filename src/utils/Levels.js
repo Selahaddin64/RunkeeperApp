d3fef5575a6b93c102454bc9bd73c8063bfd82cf
Selahattin Demir <selahattindemir686@gmail.com>
2022-01-10 09:23:38 +0300

    feat: utils folder created

---
+++ src/utils/Levels.js
@@ -0,0 +1,9 @@
+// Kilometer per level
+
+export const Levels = [
+  {id: 1, level: 'pink', kilometerRequired: 0},
+  {id: 2, level: 'orange', kilometerRequired: 10},
+  {id: 3, level: 'green', kilometerRequired: 50},
+  {id: 4, level: 'blue', kilometerRequired: 100},
+];
+
