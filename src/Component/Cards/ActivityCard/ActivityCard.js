76b82fac9ce3d7c8d36669006135cd9a04de906e
Selahattin Demir <selahattindemir686@gmail.com>
2022-01-10 09:14:31 +0300

    feat: Added ActivityCard to Component Card file

--- src/Component/Cards/ActivityCard/ActivityCard.js
+++ src/Component/Cards/ActivityCard/ActivityCard.js
@@ -7,12 +7,12 @@ import {
   pacePresentation,
   secondsToHm,
 } from '../../utils/Calculations';
-import styles from './ActivityCard.styles';
 import routes from '../../Navigation/routes';
+import styles from './ActivityCard.styles';
+
 
 const ActivityCard = props => {
   const navigation = useNavigation();
-  console.log(props);
   return (
     <Pressable
       style={styles.mainContainer}
@@ -25,37 +25,29 @@ const ActivityCard = props => {
           cal: props.cal,
         })
       }>
-      {/* Inner Container 1 */}
       <View style={styles.innerContainer1}>
-        {/* Image */}
         <Image
           source={{uri: 'https://i.stack.imgur.com/ddX9U.png'}}
           style={styles.image}
         />
         <View style={styles.headingContainer}>
-          {/* Heading */}
           <Text style={styles.heading}>{props.day}</Text>
-          {/* Subheading */}
           <Text style={styles.subheading}>
             {props.day} {props.timeOfDay} Run
           </Text>
         </View>
       </View>
-      {/* Inner Container 2*/}
       <View style={styles.innerContainer2}>
-        {/* Kilometer */}
         <View>
           <Text style={styles.metricValue}>{props.distance.toFixed(1)}</Text>
           <Text style={styles.metric}>Kilometer</Text>
         </View>
-        {/* Avg pace */}
         <View>
           <Text style={styles.metricValue}>
             {pacePresentation(calculatePace(props.distance, props.time))}
           </Text>
           <Text style={styles.metric}>Avg. Pace</Text>
         </View>
-        {/* Time */}
         <View>
           <Text style={styles.metricValue}>
             {secondsToHm(props.time).substring(0, 5)}
